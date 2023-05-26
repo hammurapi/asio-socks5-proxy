a175ccb42521ebc00c68933e45835851a3f00de3
hammurapi <3584715+hammurapi@users.noreply.github.com>
2023-05-26 23:13:29 +0200

    Changing name to socks-proxy

--- source/src/config-reader.hpp
+++ source/src/config-reader.hpp
@@ -1,59 +1,57 @@
 /**
-* @file config_reader.hpp
-* @brief Reads configuration file (like standard Linux .conf files)
-* @author philave (philave7@gmail.com)
-*/
+ * @file config_reader.hpp
+ * @brief Reads configuration file (like standard Linux .conf files)
+ * @author philave (philave7@gmail.com)
+ */
 
 #ifndef CONFIG_READER_HPP
 #define CONFIG_READER_HPP
 
-#include <map>
-#include <string>
 #include <fstream>
-#include <sstream>
+#include <map>
 #include <regex>
+#include <sstream>
+#include <string>
 
-class ConfigReader
-{
-public:
-	ConfigReader(){}
-	ConfigReader(const ConfigReader& config_reader) : settings(config_reader.settings) { }
+class ConfigReader {
+  public:
+    ConfigReader() {}
+    ConfigReader( const ConfigReader& config_reader ) : settings( config_reader.settings ) {}
 
-	void parse(std::string file_name)
-	{
-		std::ifstream configFile(file_name);
+    void parse( std::string file_name ) {
+        std::ifstream configFile( file_name );
 		std::string line;
-		while (std::getline(configFile, line))
-		{
-			size_t posComment = line.find('#');
-			if (posComment != std::string::npos) line = line.substr(0, posComment);
-			line = std::regex_replace(line, std::regex("^[ \t]*"), "");
-			if (line.size() == 0) continue;
-			line = std::regex_replace(line, std::regex("[ \t]+"), " ");
-			std::istringstream string_reader(line);
+        while( std::getline( configFile, line ) ) {
+            size_t posComment = line.find( '#' );
+            if( posComment != std::string::npos )
+                line = line.substr( 0, posComment );
+            line = std::regex_replace( line, std::regex( "^[ \t]*" ), "" );
+            if( line.size() == 0 )
+                continue;
+            line = std::regex_replace( line, std::regex( "[ \t]+" ), " " );
+            std::istringstream string_reader( line );
 			std::string key, value;
 			string_reader >> key >> value;
 			settings[key] = value;
 		}
 
 		// For test
-		//for (const auto& item : settings) std::cout << item.first << "=" << item.second << std::endl;
+        // for (const auto& item : settings) std::cout << item.first << "=" << item.second << std::endl;
 	}
 
-	bool check_key(const std::string& key)
-	{
-		auto it = settings.find(key);
-		if (it == settings.end()) return false;
+    bool check_key( const std::string& key ) {
+        auto it = settings.find( key );
+        if( it == settings.end() )
+            return false;
 		return true;
 	}
 
-	const char* get_key_value(const std::string& key)
-	{
+    const char* get_key_value( const std::string& key ) {
 		return settings[key].c_str();
-	}
-private:
-	std::map<std::string, std::string> settings;
+    }
 
+  private:
+    std::map<std::string, std::string> settings;
 };
 
 #endif // CONFIG_READER_HPP

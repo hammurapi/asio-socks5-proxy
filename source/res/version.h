7ea9e9a1f1491173b192daaeafd55f1074c4e801
hammurapi <3584715+hammurapi@users.noreply.github.com>
2023-05-26 16:04:48 +0200

    restructuring the project

---
+++ source/res/version.h
@@ -0,0 +1,17 @@
+/*
+* Copyright (C) hammurapi 2023
+* All Rights Reserved. Confidential.
+*/
+#pragma once
+
+// VER_DATE format: YEAR, MONTH, DAY, (optional 0/WEEK)
+
+#define VER_COMPANYNAME_STR "hammurapi"
+#define VER_PRODUCTNAME_STR "asio-socks5-proxy"
+
+#define VER_FILEVERSION 0,0,1,0
+#define VER_FILEVERSION_STR "0.0.1.0 (2023-05-26)"
+#define VER_DATE 2023,5,26,21
+
+#define VER_PRODUCTVERSION VER_FILEVERSION
+#define VER_PRODUCTVERSION_STR VER_FILEVERSION_STR

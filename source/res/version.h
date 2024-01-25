b8a790c971a2d7196377fcb2848b4abe258295c7
hammurapi <3584715+hammurapi@users.noreply.github.com>
2024-01-25 09:24:24 +0100

    Bumping version number

--- source/res/version.h
+++ source/res/version.h
@@ -6,12 +6,12 @@
 
 // VER_DATE format: YEAR, MONTH, DAY, (optional 0/WEEK)
 
-#define VER_COMPANYNAME_STR "philave (philave7@gmail.com)"
+#define VER_COMPANYNAME_STR "philave (philave7@gmail.com) & hammurapi (https://github.com/hammurapi)"
 #define VER_PRODUCTNAME_STR "asio-socks5-proxy"
 
-#define VER_FILEVERSION 0,0,3,1
-#define VER_FILEVERSION_STR "0.0.3.1 (2024-01-24)"
-#define VER_DATE 2024,1,24,4
+#define VER_FILEVERSION 0,0,3,2
+#define VER_FILEVERSION_STR "0.0.3.2 (2024-01-25)"
+#define VER_DATE 2024,1,25,4
 
 #define VER_PRODUCTVERSION VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR VER_FILEVERSION_STR
